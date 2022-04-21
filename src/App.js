c199b0cd2fc67a347caae1c42e3618703644c7e7
Eston Li <liyuelin2004@gmail.com>
2022-04-21 01:06:57 -0400

    Changed .gitignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className={"flex flex-col w-full min-h-screen bg-emerald-100"}>
+            <Navbar />
+
+            <div className={"container flex-grow mx-auto"}>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+                <h1>Gan</h1>
+            </div>
+        </div>
+    );
+};
 
 export default App;
